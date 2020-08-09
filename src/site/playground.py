c720a5be7755586f4944438e4a5742f5bd970b9b
Nathan Tresham <nathantresham@gmail.com>
2020-08-09 01:04:34 -0700

    added playground, output is still messy

--- src/site/playground.py
+++ src/site/playground.py
@@ -8,12 +8,24 @@ def run():
 	fname = 'bin/playground/' + str(round(time.time() * 1000))
 
 	with open(fname + '.cc', 'w+') as f:
-		f.write('''#include <iostream> int main(int argc, char *argv[]) {''')
+		f.write('''#include <iostream>\nint main(int argc, char *argv[]) {''')
 		f.write(request.json['code'])
 		f.write('''return 0;}''')
 
-	build_output = '' 
-	run_output = ''
+	build_command = 'g++ {0}.cc -o {0}.a; exit 0'.format(fname)
+	build_output = subprocess.check_output(
+		build_command, 
+		stderr=subprocess.STDOUT, 
+		shell=True
+	).decode("utf-8")
+
+	run_command = './{0}.a; exit 0'.format(fname)
+	run_output = subprocess.check_output(
+		run_command,
+		stderr=subprocess.STDOUT,
+		shell=True
+	).decode("utf-8")
+
 	response = {
 		'build_output': build_output,
 		'run_output': run_output
--- src/site/views.py
+++ src/site/views.py
@@ -1,5 +1,5 @@
 from . import app
-from flask import render_template
+from flask import render_template, request
 
 @app.route('/')
 def index():
@@ -16,6 +16,23 @@ def help():
 @app.route('/about')
 def about():
 	return render_template('about.html')
+
+@app.route('/query')
+def query():
+	query = request.args.get('i')
+	processed_query = 'Processed query goes here'
+	result = 'Result goes here'
+	declarations = []
+	for key in request.args:
+		if key == 'i':
+			continue
+		declarations.append({'name': key, 'input': request.args.get(key)})
+	return render_template('query.html', 
+		query=query,
+		processed_query=processed_query,
+		result=result,
+		declarations=declarations
+	)
 
 @app.errorhandler(404)
 def page_not_found(error):
