c283f8922fa6c827898c3e9d3103d0c89df9730d
Nathan Tresham <nathantresham@gmail.com>
2020-08-14 01:21:51 -0700

    add declarations persistence between index and query page

--- src/site/views.py
+++ src/site/views.py
@@ -3,7 +3,7 @@ from flask import render_template, request
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	return render_template('index.html', declarations=get_declarations_from_request(request))
 
 @app.route('/playground')
 def playground():
@@ -23,17 +23,21 @@ def query():
 	processed_query = 'Processed query goes here'
 	result = 'Result goes here'
 	declarations = []
-	for key in request.args:
-		if key == 'i':
-			continue
-		declarations.append({'name': key, 'input': request.args.get(key)})
 	return render_template('query.html', 
 		query=query,
 		processed_query=processed_query,
 		result=result,
-		declarations=declarations
+		declarations=get_declarations_from_request(request)
 	)
 
 @app.errorhandler(404)
 def page_not_found(error):
 	return render_template('404.html', error=error), 404
+
+def get_declarations_from_request(req):
+	declarations = []
+	for key in req.args:
+		if key == 'i':
+			continue
+		declarations.append({'name': key, 'input': req.args.get(key)})
+	return declarations
